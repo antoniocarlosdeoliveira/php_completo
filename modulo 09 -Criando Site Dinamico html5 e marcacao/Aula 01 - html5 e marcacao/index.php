6056720dea85e02eb9a971d2e2cd3a68f52bbe5a
Antonio Carlos De Oliveira <livegames2011@hotmail.com>
2020-06-07 21:26:30 -0300

    create marking html5

---
+++ modulo 09 -Criando Site Dinamico html5 e marcacao/Aula 01 - html5 e marcacao/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Projeto 01</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="html5, php, css">
+    <meta name="description" content="Descrição do meu site">
+</head>
+<body>
+    <header>
+        <div class="logo">Logomarca</div><!--logo-->
+        <nav class="desktop">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+        </nav><!--desktop-->
+        <nav class="mobile">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+        </nav><!--mobile-->
+    </header>
+    <section class="banner-principal">
+        <form>
+            <h2>Qual o seu e-mail</h2>
+            <input type="email" name="email" require />
+            <input type="submit" name="acao" value="Cadastrar!">
+        </form>
+    </section><!--banner-principal-->
+    <section class="descricao-autor">
+        <div class="w50">
+            <h2>Antonio Carlos De Oliveira</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,</p>
+        </div><!--w50-->
+        <div class="w50">
+            <!--Pegar imagem depois-->
+            <img src="" alt="" />
+        </div><!--w50-->
+    </section><!--descricao-autor-->
+</body>
+</html>
