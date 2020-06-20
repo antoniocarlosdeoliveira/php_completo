ecf9050276e533a7c58d73023a4e347fec0da404
Antonio Carlos De Oliveira <livegames2011@hotmail.com>
2020-06-20 14:45:45 -0300

    Criado a marcação do site

--- modulo 09 -Criando Site Dinamico html5 e marcacao/Aula 01 - html5 e marcacao/index.php
+++ modulo 09 -Criando Site Dinamico html5 e marcacao/Aula 01 - html5 e marcacao/index.php
@@ -9,41 +9,114 @@
 </head>
 <body>
     <header>
-        <div class="logo">Logomarca</div><!--logo-->
-        <nav class="desktop">
-            <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Sobre</a></li>
-                <li><a href="">Serviços</a></li>
-                <li><a href="">Contato</a></li>
-            </ul>
-        </nav><!--desktop-->
-        <nav class="mobile">
-            <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Sobre</a></li>
-                <li><a href="">Serviços</a></li>
-                <li><a href="">Contato</a></li>
-            </ul>
-        </nav><!--mobile-->
+        <div class="center">
+            <div class="logo">Logomarca</div><!--logo-->
+            <nav class="desktop">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav><!--desktop-->
+            <nav class="mobile">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav><!--mobile-->
+        </div><!--center-->
     </header>
     <section class="banner-principal">
-        <form>
-            <h2>Qual o seu e-mail</h2>
-            <input type="email" name="email" require />
-            <input type="submit" name="acao" value="Cadastrar!">
-        </form>
+        <div class="center">
+            <form>
+                <h2>Qual o seu e-mail</h2>
+                <input type="email" name="email" require />
+                <input type="submit" name="acao" value="Cadastrar!">
+            </form>
+        </div><!--center-->
     </section><!--banner-principal-->
     <section class="descricao-autor">
-        <div class="w50">
-            <h2>Antonio Carlos De Oliveira</h2>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,</p>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,</p>
-        </div><!--w50-->
-        <div class="w50">
-            <!--Pegar imagem depois-->
-            <img src="" alt="" />
-        </div><!--w50-->
+        <div class="center">
+            <div class="w50">
+                <h2>Antonio Carlos De Oliveira</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,</p>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,</p>
+            </div><!--w50-->
+            <div class="w50">
+                <!--Pegar imagem depois-->
+                <img src="" alt="" />
+            </div><!--w50-->
+        </div><!--center-->
     </section><!--descricao-autor-->
+
+    <section class="especialidades">
+
+        <div class="center">
+            <div class="box-especialidade">
+            <h2 class="title">Especialidades</h2>
+                <h3>Icone1</h3>
+                <h3>CSS3</h3>
+                <p>O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500,</p>
+            </div><!--box-especialidade-->
+
+            <div class="box-especialidade">
+                <h3>Icone2</h3>
+                <h3>HTML5</h3>
+                <p>O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500,</p>
+            </div><!--box-especialidade-->
+
+            <div class="box-especialidade">
+                <h3>Icone3</h3>
+                <h3>JavaScript</h3>
+                <p>O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500,</p>
+            </div><!--box-especialidade-->
+        </div>
+    </section>
+    <section class="extras">
+        <div class="center">
+            <div class="w50">
+            
+                <h2 class="title">Depoimentos</h2>
+                <div class="depoimento-single">
+                    <p>É um facto estabelecido de que um leitor é distraído pelo conteúdo legível de uma página quando analisa a sua mancha gráfica. Logo, o uso de Lorem Ipsum leva a uma distribuição mais ou menos normal de letras, ao contrário do uso de "Conteúdo aqui, conteúdo aqui", tornando-o texto legível. </p>
+                    <p class="nome-autor">Lorem Ipsum</p>
+                </div><!--depoimento-single-->
+
+                <div class="depoimento-single">
+                    <p>É um facto estabelecido de que um leitor é distraído pelo conteúdo legível de uma página quando analisa a sua mancha gráfica. Logo, o uso de Lorem Ipsum leva a uma distribuição mais ou menos normal de letras, ao contrário do uso de "Conteúdo aqui, conteúdo aqui", tornando-o texto legível. </p>
+                    <p class="nome-autor">Lorem Ipsum</p>
+                </div><!--depoimento-single-->
+                
+                <div class="depoimento-single">
+                    <p>É um facto estabelecido de que um leitor é distraído pelo conteúdo legível de uma página quando analisa a sua mancha gráfica. Logo, o uso de Lorem Ipsum leva a uma distribuição mais ou menos normal de letras, ao contrário do uso de "Conteúdo aqui, conteúdo aqui", tornando-o texto legível. </p>
+                    <p class="nome-autor">Lorem Ipsum</p>
+                </div><!--depoimento-single-->
+
+            </div><!--w50-->
+
+            <div class="w50">
+                <h2 class="title">Serviços</h2>
+                <div class="servicos">
+                    <ul>
+                        <li>É um facto estabelecido de que um leitor é distraído pelo conteúdo legível de uma página quando analisa a sua mancha gráfica. Logo, o uso de Lorem Ipsum leva a uma distribuição mais ou menos normal de letras, ao contrário do uso de "Conteúdo aqui, conteúdo aqui", tornando-o texto legível.</li>
+
+                        <li>É um facto estabelecido de que um leitor é distraído pelo conteúdo legível de uma página quando analisa a sua mancha gráfica. Logo, o uso de Lorem Ipsum leva a uma distribuição mais ou menos normal de letras, ao contrário do uso de "Conteúdo aqui, conteúdo aqui", tornando-o texto legível.</li>
+
+                        <li>É um facto estabelecido de que um leitor é distraído pelo conteúdo legível de uma página quando analisa a sua mancha gráfica. Logo, o uso de Lorem Ipsum leva a uma distribuição mais ou menos normal de letras, ao contrário do uso de "Conteúdo aqui, conteúdo aqui", tornando-o texto legível.</li>
+                    </ul>
+                </div>
+
+            </div><!--w50-->
+        </div><!--center-->
+    </section><!--extras-->
+
+    <footer>
+        <div class="center">
+            <p>Todos os direitos reservados</p>
+        </div><!--center-->
+    </footer>
 </body>
 </html>
